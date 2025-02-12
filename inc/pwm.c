75e9335b138c314125c8581fc3f6c60348858eaa
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-02-11 23:09:31 -0300

    Correção de bugs

--- inc/pwm.c
+++ inc/pwm.c
@@ -6,10 +6,10 @@ void pwm_init_all(uint8_t pwm_pin, float clkdiv, uint16_t wrap)
 {
     gpio_set_function(pwm_pin, GPIO_FUNC_PWM);
     uint slice = pwm_gpio_to_slice_num(pwm_pin);
-    pwm_set_clkdiv(pwm_pin, clkdiv);
-    pwm_set_wrap(pwm_pin, wrap);
-    pwm_set_enabled(slice, true);
+    pwm_set_clkdiv(slice, clkdiv);
+    pwm_set_wrap(slice, wrap);
     pwm_set_gpio_level(pwm_pin, 0);
+    pwm_set_enabled(slice, true);
 }
 
 void pwm_set_duty_cycle(uint8_t pwm_pin, uint duty_cycle)
